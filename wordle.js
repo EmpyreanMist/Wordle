e011fa565601da3b34cac5ae20c691f424bf997e
Christian Fryksten <christianfryksten1@gmail.com>
2025-03-18 11:26:00 +0100

    Task breakdown instructions

---
+++ wordle.js
@@ -0,0 +1,13 @@
+/* 
+1. Gör en funktion som tar emot 2 parametrar "guess" och "correctWord"
+2. Skapa en array som ska sedan ska innehålla objekt "feedback"
+3. Ta bort allt som inte är A-Z i guess och correctWord
+3. Kolla om "guess" och "correctWord" är 5 bokstäver annars returnera och ge feedback på att de behöver vara 5 karaktärer 
+4. Gör guess och correctWord till versaler
+5. Gör "guess" till array värden för varje bokstav
+6. Loopa igenom guess arrayen och jämför varja bokstav med correctWord beroende på skillanderna: pusha in i feedback ett av följande:
+ " K / incorrect"
+ " K / misplaced"
+ " K / correct"
+7. Returnera feedBack
+ */
