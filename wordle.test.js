2ce5a9ae90c482d3de829e364ebec295f376c80f
Christian Fryksten <christianfryksten1@gmail.com>
2025-03-19 18:35:51 +0100

    Add more test for the wordle algoritm

--- wordle.test.js
+++ wordle.test.js
@@ -9,4 +9,22 @@ describe('getWordleFeedback()', () => {
     expect(output[0]).toHaveProperty('error');
   });
 
+/* Testar också om man får ett objekt i en array som innehåller texten error fast när man matar in det korrekta ordets längd fel */
+  it('should give an error message when correctWord is not 5 characters', () => {
+    const output = getWordleFeedback('abcde', 'abc');
+    expect(output[0]).toHaveProperty('error');
+  });
+
+/* Testar att svaret från algoritmen bekräftar när man har gjort allt rätt för varje bokstav */
+  it('should confirm all letters as correct when guess and correctAnswer is the same', () => {
+    const output = getWordleFeedback('learn', 'learn');
+    expect(output).toEqual([
+      { letter: 'L', result: 'correct'},
+      { letter: 'E', result: 'correct'},
+      { letter: 'A', result: 'correct'},
+      { letter: 'R', result: 'correct'},
+      { letter: 'N', result: 'correct'}
+  ]);
+  });
+
 });
